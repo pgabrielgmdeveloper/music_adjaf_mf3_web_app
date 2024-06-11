e84da1443cea35458429aa8f8df3f48347f52b65
paulo <pgabrieldeveloper@outlook.com>
2024-06-10 22:39:23 -0300

    feat(service): adicionando post para criação de musicas

--- src/app/services/music-api.service.ts
+++ src/app/services/music-api.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {CultResponse} from "../model/cult/cult";
+import { CreateMusic } from '../model/music/music';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,6 +16,17 @@ export class MusicApiService {
 
   get getCultsDay() : Observable<Array<CultResponse>> {
     return this.http.get<Array<CultResponse>>(`${this.url}/cult`).pipe(
+      res => res,
+      err => err
+    );
+  }
+
+  createMusic(payload: CreateMusic): Observable<any> {
+    const formData = new FormData();
+    const musicJson = {name: payload.name,singer: payload.singer, letter: payload.letter}
+    formData.append("music", payload.music!);
+    formData.append("musicJson", JSON.stringify(musicJson));
+    return this.http.post<any>(`${this.url}/music`, formData).pipe(
       res => res,
       err => err
     );
